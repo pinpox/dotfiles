8d021799f7eaecc18ff2f66f1b85f727c72b807a
Pablo <pablo1@mailbox.org>
2018-12-16 15:46:28 +0100

    updated files from zitrone

---
+++ .zsh_plugins.sh
@@ -0,0 +1,34 @@
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/oh-my-zsh.sh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto/init.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sorin-ionescu-SLASH-prezto )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( /home/binaryplease/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages )
