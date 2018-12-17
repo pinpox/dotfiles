893833ad07aa4c74fbb8645f3a41859e30d0669b
Pablo <pablo1@mailbox.org>
2018-12-17 23:00:41 +0100

    Set to custom colors theme based on snazzy

--- bootstrap.sh
+++ bootstrap.sh
@@ -136,6 +136,6 @@ confirmExecute "Setup VIM/Neovim? [Y/n]" setup_vim
 confirmExecute "Set ZSH as shell? [Y/n]" chsh -s /bin/zsh
 confirmExecute "Install antibody? [Y/n]" install_antibody
 confirmExecute "Install termite terminfo? [Y/n]" tic -x ./local/share/termite.terminfo
-confirmExecute "Setup colors with base16-manager (base16-$BASE16_THEME)? [Y/n]" setup_colors
+# confirmExecute "Setup colors with base16-manager (base16-$BASE16_THEME)? [Y/n]" setup_colors
 confirmExecute "Setup git configuration? [Y/n]" setup_git
 
