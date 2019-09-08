683e03dced0af0cf51b3eb5ac470e9f72fd59cf7
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-09-08 22:16:33 +0200

    added fish shell configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
