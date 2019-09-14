9d36c8e621b3ad664abf78ebb06c0770c123eb72
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-09-14 13:54:46 +0200

    base16-tomorrow-night theme, base16-shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,9 @@ if status is-login
     end
 end
 
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
