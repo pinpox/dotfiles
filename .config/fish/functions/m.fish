82dab2f50dd1faa178548846dcf27a0d9804e283
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2019-09-16 10:37:45 +0200

    added all fish files

--- .config/fish/functions/m.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function m --description 'alias m=neomutt'
-	neomutt  $argv;
-end
--- .config/fish/functions/y.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function y --description 'alias y=yay'
-	yay  $argv;
-end
